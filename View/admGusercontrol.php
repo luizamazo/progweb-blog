19b8c696c27045c78c10de8f61531342a5523f24
mayarabassanelli <mayarabassanelli@hotmail.com>
2018-09-21 16:28:34 -0400

    tela adm geral arrumado html

--- View/admGusercontrol.php
+++ View/admGusercontrol.php
@@ -20,19 +20,48 @@
 
 
 	<body>
-    <div class="centro">
-         <div class="box_centro" align="center">
-             <div><h1> ADM G - User Control</h1></div>
+    <div>
+        <label id="titulo">12 Horas de Terror</label>
+        <br>
+        <label>Um blog para os amantes do dark side.</label>
+    </div>
 
-             <table border="1">
-                <tr>
-                    <th>Identificador</th>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Tipo</th>
-                    <th>Estado</th>
-                    <!--<th>Ações</th>-->
-                </tr>
+    <br>
+
+    <div id="menu">
+        <ul>
+            <li><a href="inicio.html">INÍCIO</a></li>
+            <li><a href="inicio.html">POSTAGENS</a></li>
+            <li><a href="teste.html">SUA CONTA</a></li>
+        </ul>
+
+        <hr id="traco">
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <div id="corpo_texto2">
+        
+      <div class="centro3">
+
+        <label id="donoconta"> ADM G - User Control</label>
+        <br>
+        <br>  
+             
+        <table id="tabela1">
+          <tr>
+            <th>Identificador</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Tipo</th>
+            <th>Estado</th>
+            <!--<th>Ações</th>-->
+          </tr>
 
                 
                     <?php 
@@ -55,42 +84,97 @@
                     ?>      
              </table>
 
-             <div><h1> Editar Usuário</h1></div>
-                 
-                 <div>
-                 <form action="/progweb-blog/class/userCRUD.php" method="post">
-                     
-                     <input type="text" name="cod" placeholder="Código Identificador"><br>
-                     <input type="text" name="nome" placeholder="Novo nome"><br>
-                     <input type="email" name="email" placeholder="Novo email"><br>
-                     <input type="password" name="senha" placeholder="Nova senha"><br>
-                     <select name="tipo" id="permissao">
-                         <option value="0" selected="selected">Alterar Permissão</option>
-                         <option value="1">Adm Geral</option>
-                         <option value="2">Adm</option>
-                         <option value="3">Redator</option>
-                         <option value="4">Usuário</option>
-                     </select>
-                     
-                     <input type="submit" name="submit" value="Salvar"><br>
-                 </form>
-                 </div>
+             <br>
+        <br>
 
-                  <div><h1> Excluir Usuário</h1></div>
+        <div id="alinhar1">
+            
+          <div id="menueditar">
+
+             <label id="subtitulo">Editar Usuário</label>
+             <br>
+                                  
+                <form action="/progweb-blog/class/userCRUD.php" method="post">
+                       
+                  <table>  
+                    <tr>
+                      <td>
+                        <input type="text" name="cod" placeholder="Código Identificador"><br>
+                      </td>
+
+                      <td>
+                        <input type="text" name="nome" placeholder="Novo Nome"><br>  
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        <input type="email" name="email" placeholder="Novo Email"><br>
+                      </td>
+
+                      <td>
+                        <input type="password" name="senha" placeholder="Nova Senha"><br>  
+                      </td>
+                    </tr>
+                    
+                    <tr>
+                      <td>
+                        <select name="tipo" id="permissao">
+                          <option value="0" selected="selected">Alterar Permissão</option>
+                          <option value="1">Adm Geral</option>
+                          <option value="2">Adm</option>
+                          <option value="3">Redator</option>
+                          <option value="4">Usuário</option>
+                        </select>
+                      </td>
+
+                      <td>
+                        <input type="submit" name="submit" value="Salvar" id="botao2"><br>
+                      </td>
+                    </tr> 
+                 </table>
+
+                </form>
+          </div>
+
+
+
+
+
+          <div id="menuexcluir">
+
+            <label id="subtitulo">Excluir Usuário</label>
+            <br>
                  
-                 <div>
-                 <form action="/progweb-blog/class/userCRUD.php" method="post">
-                     
-                     <input type="text" name="cod" placeholder="Código Identificador"><br>
-                     <input type="text" name="email" placeholder="Email"><br>
-                     
-                     <input type="submit" name="submit" value="Salvar"><br>
-                 </form>
-                 </div>
+            <form action="/progweb-blog/class/userCRUD.php" method="post">
+             
+                <table align="center">
+                  
+                    <tr>     
+                      <td>  
+                        <input type="text" name="cod" placeholder="Código Identificador"><br>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>  
+                        <input type="text" name="email" placeholder="Email"><br>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>  
+                        <input type="submit" name="submit" value="Salvar" id="botao1"><br>
+                      </td>                     
+                    </tr>
+                 
+                </table>
+            </form>
         
-         </div>
+          </div>
+             
+        </div>
 
     </div>
+
 
 	</body>
 </html>
