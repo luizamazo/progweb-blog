9d6f8f607559fc6539d91c3c2197fee090d83b62
mayarabassanelli <mayarabassanelli@hotmail.com>
2018-09-21 19:15:12 -0400

    tela user index com html

--- View/userIndex.php
+++ View/userIndex.php
@@ -20,22 +20,49 @@
 
 
 	<body>
-    <div class="centro">
-         <div class="box_centro" align="center">
-             <div><h1> USER </h1></div>
-           
-         <div>
-      
-            
-            <ul>
-                <li><a href="/progweb-blog/View/inicial.html">Ver Posts do Blog</a></li>
-                <li><a href="/progweb-blog/View/userControl.php">Gerenciar Comentários</a></li>
-            </ul>
-
-         </div>
-   
+    <div>
+        <label id="titulo">12 Horas de Terror</label>
+        <br>
+        <label>Um blog para os amantes do dark side.</label>
     </div>
 
+    <br>
+
+    <div id="menu">
+        <ul>
+            <li><a href="inicio.html">INÍCIO</a></li>
+            <li><a href="inicio.html">POSTAGENS</a></li>
+            <li><a href="login.html">SUA CONTA</a></li>
+        </ul>
+
+        <hr id="traco">
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <div id="corpo_texto3">
+        
+      <div class="centro4">
+
+        <label id="donoconta">USER</label>
+        <br>
+        <br>
+
+        <div>
+          <ul id="gerencia1">
+            <li id="li1"><a href="/progweb-blog/View/inicial.html"id="link1">Ver Posts do Blog</a></li>
+            <li id="li1"><a href="/progweb-blog/View/userControl.php" id="link1">Gerenciar comentários</a></li>
+          </ul>  
+        </div>
+             
+      </div>
+
+    </div>  
 	</body>
 </html>
 
