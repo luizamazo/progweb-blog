f11a08062dc689520e0c0947c79a0f96f2b9a71d
mayarabassanelli <mayarabassanelli@hotmail.com>
2018-09-21 19:00:12 -0400

    tela redator index com html

--- View/redaIndex.php
+++ View/redaIndex.php
@@ -20,21 +20,51 @@
 
 
 	<body>
-    <div class="centro">
-         <div class="box_centro" align="center">
-             <div><h1> REDATOR </h1></div>
-           
-         <div>
-      
-            
-            <ul>
-                <li><a href="/progweb-blog/View/createPosts.php">Criar Posts</a></li>
-                <li><a href="">Gerenciar comentários</a></li>
-            </ul>
-
-         </div>
-   
+    
+    <div>
+        <label id="titulo">12 Horas de Terror</label>
+        <br>
+        <label>Um blog para os amantes do dark side.</label>
     </div>
+
+    <br>
+
+    <div id="menu">
+        <ul>
+            <li><a href="inicio.html">INÍCIO</a></li>
+            <li><a href="inicio.html">POSTAGENS</a></li>
+            <li><a href="login.html">SUA CONTA</a></li>
+        </ul>
+
+        <hr id="traco">
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <div id="corpo_texto3">
+        
+      <div class="centro4">
+
+        <label id="donoconta">REDATOR</label>
+        <br>
+        <br>
+
+        <div>
+          <ul id="gerencia1">
+            <li id="li1"><a href="/progweb-blog/View/createPosts.php" id="link1">Criar Posts</a></li>
+            <li id="li1"><a href="" id="link1">Gerenciar Comentários</a></li>
+        </ul>  
+      </div>
+             
+      </div>
+
+    </div>   
+
 
 	</body>
 </html>
