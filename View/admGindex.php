3164ea12a0e0c914d7be57dde81d0794c200f175
mayarabassanelli <mayarabassanelli@hotmail.com>
2018-09-21 19:34:03 -0400

    tela adm g index com html

--- View/admGindex.php
+++ View/admGindex.php
@@ -20,23 +20,52 @@
 
 
 	<body>
-    <div class="centro">
-         <div class="box_centro" align="center">
-             <div><h1> ADMIN G</h1></div>
-           
-         <div>
-      
-            
-            <ul>
-                <li><a href="/progweb-blog/View/admGadd.php">Adicionar Usuários</a></li>
-                <li><a href="/progweb-blog/View/admGusercontrol.php">Gerenciar Usuários</a></li>
-                <li><a href="">Gerenciar Posts</a></li>
-                <li><a href="">Gerenciar comentários?</a></li>
-            </ul>
-
-         </div>
-   
+    
+    <div>
+        <label id="titulo">12 Horas de Terror</label>
+        <br>
+        <label>Um blog para os amantes do dark side.</label>
     </div>
+
+    <br>
+
+    <div id="menu">
+        <ul>
+            <li><a href="inicio.html">INÍCIO</a></li>
+            <li><a href="inicio.html">POSTAGENS</a></li>
+            <li><a href="login.html">SUA CONTA</a></li>
+        </ul>
+
+        <hr id="traco">
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <div id="corpo_texto4">
+        
+      <div class="centro5">
+
+        <label id="donoconta">ADMIN G</label>
+        <br>
+        <br>
+
+        <div>
+          <ul id="gerencia1">
+            <li id="li1"><a href="/progweb-blog/View/admGadd.php" id="link1">Adicionar Usuários</a></li>
+            <li id="li1"><a href="/progweb-blog/View/admGusercontrol.php" id="link1">Gerenciar Usuários</a></li>
+            <li id="li1"><a href="" id="link1">Gerenciar Posts</a></li>
+            <li id="li1"><a href="" id="link1">Gerenciar Comentários</a></li>
+          </ul>  
+        </div>
+             
+      </div>
+
+    </div>  
 
 	</body>
 </html>
