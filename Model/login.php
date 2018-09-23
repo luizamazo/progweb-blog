19177056019a3b75151f804441ad8e7b05b579e9
Luiza <luizamazo@hotmail.com>
2018-09-23 04:10:06 -0300

    caminho

--- Model/login.php
+++ Model/login.php
@@ -3,16 +3,23 @@
 require_once("../config.php");
 include_once "../class/auth.php";
 
-	$link = new sql();
-	$conn = $link->createConn();
+class login{
 	
-	$email = $_POST['email-input'];
-	$senha = $_POST['senha-input'];
-	$phash = md5($senha);
-
-	function userLogin($email, $phash){
+	
+	private $email;
+	private $senha;
+	private $phash;
+	
+	public function userLogin(){
 		
-			$sql = new sql();
+			$link = new sql();
+			$conn = $link->createConn();
+
+			if(isset($_POST['email-input']) && isset($_POST['senha-input'])){
+				$this->email = $_POST['email-input'];
+				$this->senha = $_POST['senha-input'];
+				$this->phash = md5($senha);
+				$sql = new sql();
 			$result = $sql->select("SELECT tipo, nome, estado FROM pessoa WHERE email = :EMAIL AND senha = :SENHA",
 			array(
 				":EMAIL"=>$email,
@@ -20,25 +27,30 @@ include_once "../class/auth.php";
 			));
 
 			
-		if($result == false){ //if(count($result)  > 0 ){
-			echo "Email ou senha incorretos";
-		}else{
-			$estado = $result[0]["estado"];
-			if($estado != 2){
-				$_SESSION['tipo'] = $result[0]["tipo"];
-				$_SESSION['nome'] = $result[0]["nome"];
-				$_SESSION['logado'] = true;
-				$auth = new auth();
-				$auth->authIndex();
+			if($result == false){ //if(count($result)  > 0 ){
+				echo "Email ou senha incorretos";
 			}else{
-				echo "Essa conta foi deletada!";
+				$estado = $result[0]["estado"];
+				if($estado != 2){
+					$_SESSION['tipo'] = $result[0]["tipo"];
+					$_SESSION['nome'] = $result[0]["nome"];
+					$_SESSION['logado'] = true;
+					$auth = new auth();
+					$auth->authIndex();
+				}else{
+					echo "Essa conta foi deletada!";
+				}
+		
 			}
-	
 		}
+
+			
 		
 	}
 
-	userLogin($email, $phash);
+}
+	$stmt = new login();
+	$stmt->userLogin();
    
 
 ?>
